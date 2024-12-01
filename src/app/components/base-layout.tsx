438ee171c031c97cbe59ec78cca5638e077a949f
rizkyIqbal <arizkyiqbal@gmail.com>
2024-12-01 16:57:26 +0700

    install project

---
+++ src/app/components/base-layout.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React from "react";
+import Navbar from "./navbar"
+// import Footer from "./footer";
+
+export default function BaseLayout({
+    children,
+}: Readonly<{ children: React.ReactNode }>) {
+    return (
+        <>
+            <Navbar />
+            {children}
+        </>
+    );
+}
---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <section className='body-font font-nunito w-screen h-[100px] bg-primary'>
+            
+        </section>
+    )
+}
+
+export default Navbar
